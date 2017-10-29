dc18f7fb73b57845ee0d3f2bac4ab83978028900
jpwong <jamessswong@163.com>
2017-10-29 22:07:10 +0800

    remove debug logs

--- app/src/main/java/com/example/huangjianpeng/tourguide/MainActivity.java
+++ app/src/main/java/com/example/huangjianpeng/tourguide/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i("test","oncreat");
-
         TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -26,21 +24,4 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("test","onstop");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i("test","onRestart");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("test","onDestroy");
-    }
 }
